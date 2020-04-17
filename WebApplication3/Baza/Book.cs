bb28c023faddb78c766d019f673febdfed4e4457
PlatformyBirPreis <242050@student.pwr.edu.pl>
2020-04-17 11:30:53 +0200

    Add project files.

---
+++ WebApplication3/Baza/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Baza
+{
+    public class Books
+    {
+        private ConnectionDB dbcon;
+        public int idBooks { get; set; }
+        public int idReader { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Edition { get; set; }
+    }
+}
---
+++ WebApplication3/Baza/ConnectionDB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace WebApplication3.Baza
+{
+
+    public class ConnectionDB
+    {
+        public string ConnectionString { get; set; }
+
+        public ConnectionDB(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+        public List<Books> GetAllBooks()
+        {
+            List<Books> list = new List<Books>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from books where idBooks < 3", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Books()
+                        {
+                            idBooks = Convert.ToInt32(reader["idBooks"]),
+                            idReader = Convert.ToInt32(reader["idReader"]),
+                            Title = reader["Title"].ToString(),
+                            Author = reader["Author"].ToString(),
+                            Edition = Convert.ToInt32(reader["Edition"]),
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+    }
+
+}
